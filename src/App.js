7b4c39048bd2ce8bc1c765c5861b92fcada3efc8
Paul Llanque <llanquepol@gmail.com>
2023-02-26 11:43:27 -0500

    add style to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import fccLogo from './image/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='fcc-logo-container'>
+        <img src={fccLogo} alt='logo de fcc' className='fcc-logo' />
+      </div>
     </div>
   );
 }
