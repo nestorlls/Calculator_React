c07a9f44df4a58ef8e9850e48161baaca4332f4f
Paul Llanque <llanquepol@gmail.com>
2023-02-26 17:46:50 -0500

    add pantalla

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import fccLogo from './image/freecodecamp-logo.png';
 import Boton from './components/button';
+import Pantalla from './components/Pantalla';
 
 function App() {
   return (
@@ -9,13 +10,31 @@ function App() {
         <img src={fccLogo} alt='logo de fcc' className='fcc-logo' />
       </div>
       <div className='container-calculator'>
+        <Pantalla />
         <div className='row'>
           <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
           <Boton>+</Boton>
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className='row'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='row'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='row'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
         <div className='row'></div>
       </div>
     </div>
---
+++ src/components/Pantalla.jsx
@@ -0,0 +1,7 @@
+import '../styleSheets/Pantalla.css'
+
+const Pantalla = ({ input }) => {
+  return  <div className='input'>{input}</div>;
+};
+
+export default Pantalla;
