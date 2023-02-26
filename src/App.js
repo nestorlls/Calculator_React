8aba0bfc4c52cf3742cd245d45edde830117926b
Paul Llanque <llanquepol@gmail.com>
2023-02-26 12:28:10 -0500

    add method isOperator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import fccLogo from './image/freecodecamp-logo.png';
+import Boton from './components/button';
 
 function App() {
   return (
     <div className='App'>
       <div className='fcc-logo-container'>
         <img src={fccLogo} alt='logo de fcc' className='fcc-logo' />
+      </div>
+      <div className='container-calculator'>
+        <Boton>1</Boton>
+        <Boton>+</Boton>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
       </div>
     </div>
   );
---
+++ src/components/button.jsx
@@ -0,0 +1,15 @@
+function Boton(props) {
+  const isOperator = (value) => {
+    return isNaN(value) && value !== '.' && value !== '=';
+  };
+  return (
+    <div
+      className={`boton-container ${
+        isOperator(props.children) ? 'operator' : ''
+      }`.trim()}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Boton;
